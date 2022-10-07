1b614dbe31e0e7bff5175eb7933fcb913deaa7cd
PTChinh <9a2.phamtranchinh@gmail.com>
2022-10-07 16:55:57 +0700

    Update market item command

--- contracts/library/MarketItemDetails.sol
+++ contracts/library/MarketItemDetails.sol
@@ -5,8 +5,8 @@ library MarketItems {
     struct MarketItemDetails {
         uint256 tokenId; // token id
         uint256 price; // price in USDT or Token
-        uint8 itemType; // 0: character, 1: skin, 2: items
-        uint8 rarity;   // rarity of market item
+        uint8 itemType; // 1: character, 2: skin, 3: items,...
+        uint8 rarity;   // rarity of market item []
         bool isOnMarket; // false: still private, true: on market
         address ownerBy;  // Owner token before on chain for marketplace.
     }
