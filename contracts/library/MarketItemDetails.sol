3d9d0c964ef4140cb8bc064c537760440a1669cb
PTChinh <9a2.phamtranchinh@gmail.com>
2022-10-10 11:00:01 +0700

    Update set max rarities and item type

--- contracts/library/MarketItemDetails.sol
+++ contracts/library/MarketItemDetails.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.0;
 library MarketItems {
     struct MarketItemDetails {
         uint256 tokenId; // token id
-        uint256 price; // price in USDT or Token
         uint8 itemType; // 1: character, 2: skin, 3: items,...
-        uint8 rarity;   // rarity of market item []
+        uint8 rarity;   // rarity of market item [common, rare, mythical, legendary, immotal]
         bool isOnMarket; // false: still private, true: on market
         bool isUse; // false: not use, true: used
         address ownerBy;  // Owner token before on chain for marketplace.
