b7614f10441e486fe9fc6436d51042abb49aa287
Pham Anh Tai <phamanhtai0410@gmail.com>
2023-03-13 10:17:53 +0700

    new smc

--- contracts/interfaces/IBoxFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IBoxFactory {
-    function getBoxesConfigurations() external returns(address);
-    function getBoxCreator() external returns(address);
-    function NftCollection(address _boxAddress) external view returns(address);
-}
--- contracts/interfaces/IBoxNFTCreator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IBoxNFTCreator  {
-    /**
-     *      @dev Function get boxPrice
-     */
-    function getBoxPrice(address _boxAddress) external view returns (uint256);
-}
